8112ed9058239ffbc410233625f265727578ae67
ss23094527 <ss23094527@gmail.com>
2023-02-21 12:25:13 +0800

    利用yarn安裝bootstrap套件

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import  'bootstrap/dist/css/bootstrap.min.css' 
 import App from './App'
-import './bootstrap.min.css'
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
